209433cdb3e6e2acbaa236a9324e5b9e7445b2f6
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2025-05-27 17:50:20 +0100

    update

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const connect_DB = require('./connectDB/connectDB');
 const app = express()
 
   const corsOptions = {
-  origin: process.env.CORS_ORIGIN || "*",
+  origin: "http://localhost:5175/"  ||"*" || "https://registration-app-kb3b.vercel.app/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: [
