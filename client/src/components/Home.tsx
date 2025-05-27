fc827acc00f231017f4fa10636b1a47167e6584a
Chijioke Elijah <elijahchijioke.ce@gmail.com>
2025-05-27 18:00:08 +0100

    .

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -32,8 +32,8 @@ export default function Home() {
         e.preventDefault();
         try {
             if(formData.email === formData.ConfirmEmail){
-                //await axios.post("https://server-90xsgbvqa-chijiokeelijahs-projects.vercel.app/user/register", formData);
-                await axios.post("http://localhost:3000/user/register", formData);
+                await axios.post("https://server-basx4c5n6-chijiokeelijahs-projects.vercel.app/user/register", formData);
+                // await axios.post("http://localhost:3000/user/register", formData);
             
                 console.log(formData)
                 //http://localhost:3000/user/register
